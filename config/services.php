278f20712914e3b3221e64a4b23cf888e8270053
Misha Vlasov <Vlasovm452@gmail.com>
2022-04-22 14:36:31 +0300

    make it better

--- config/services.php
+++ config/services.php
@@ -38,7 +38,7 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'http://localhost:8002/callback/google',
+        'redirect' => env(APP_URL) . '/callback/google',
         'webhook_uri' => env('GOOGLE_WEBHOOK_URI'),
         'scopes' => [
             \Google_Service_Oauth2::USERINFO_EMAIL,
